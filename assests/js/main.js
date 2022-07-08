d8c17c0ab08b54bbe0b9eae5429b1fb6ebb7e434
pooja-369 <pujagoyal369@gmail.com>
2022-07-08 22:47:58 +0530

    footer update

---
+++ assests/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('.filter-items').click(function() {
+        const value = $(this).attr('data-filter');
+        if (value == 'all') {
+            $('.post-box').show('1000');
+        } else {
+            $('.post-box').not('.' + value).hide('1000');
+            $('.post-box').filter('.' + value).show('1000');
+        }
+
+    });
+
+    $('.fliter-items').click(function() {
+        $(this).addClass("active-filter").sibling().removeClass('active-filter');
+    });
+});
+
+//header background change on scroll
+let header = document.querySelector('header')
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
